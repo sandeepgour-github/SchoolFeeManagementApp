080866313402c27143f0f85009b95f00ed7f0265
Sandeep Gour <sandeepgour597@gmail.com>
2025-12-19 13:26:08 +0530

    Add Table annotation to User entity

--- src/main/java/in/sandeep/entity/User.java
+++ src/main/java/in/sandeep/entity/User.java
@@ -2,6 +2,7 @@ package in.sandeep.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
